8c69f04ea4d52c8f572f1220250a0060b99b481a
dosang12 <knight2671@gmail.com>
2025-07-08 13:24:27 +0900

    footer finish

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
 import './Footer.css';
-
+import logo from '../images/logo.png';
 const Footer = () => {
-  return (
-    <section id="Footer" className="Footer-container">
-      Footer
-    </section>
-  );
+  return (
+    <footer className="footer-container">
+      <div className="footer-logo">
+      <img src={logo} alt="회사 로고" className="footer-logo" />
+      </div>
+      <div className="footer-info-group">
+        <p>경기 김포시 양촌읍 황금로 117 양촌지방산업단지 E블럭 1롯트 메카존 제나동 비 131호</p>
+        <span className="divider">|</span>
+        <p>대표자 : 오근승</p>
+        <span className="divider">|</span>
+        <p>사업자등록번호 : 136-09-74143</p>
+      </div>
+      <div className="footer-info-group">
+        <p>전화 : 010-5240-9641 / 010-3703-8356</p>
+        <span className="divider">|</span>
+        <p>팩스 : 031-987-9249</p>
+        <span className="divider">|</span>
+        <p>이메일 : kyungil9641@hanmail.net</p>
+      </div>
+      <div className="footer-copy">
+        Copyright © 2025 Kyungil Airshaft Inc. All rights reserved.
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
